1887d32a1d6557104d6ac5b1ff511a3e596cf4d1
PierreFAYOL <pierre.fayol14@gmail.com>
2021-09-19 16:22:27 +0200

    finition du questionnaire, ajout des explications audio
    TicTac de Suzanne, chapeaux et ballons attrapables

--- Assets/Scripts/Quiz.cs
+++ Assets/Scripts/Quiz.cs
@@ -27,12 +27,12 @@ public class Quiz : MonoBehaviour
         List<string> l;
         l = new List<string>();
         l.Add("-Granite");l.Add("-Marbre du Nil");l.Add("-Granodiorite");l.Add("-Diorite");
-        questions.Add(("De quel matériaux est faite la pierre de Rosette ? ",l,2));
+        questions.Add(("De quel matériaux est faite la pierre de Rosette ?",l,2));
 
         //Question 2
         l = new List<string>();
         l.Add("-1695");l.Add("-1790");l.Add("-1797");l.Add("-1802");
-        questions.Add(("Quand est né Champollion ? ",l,1));
+        questions.Add(("Quand est né Champollion ?",l,1));
 
         //Question 3
         l = new List<string>();
@@ -42,21 +42,42 @@ public class Quiz : MonoBehaviour
         //Question 4
         l = new List<string>();
         l.Add("-Ptolémée V ");l.Add("-Ramsès II");l.Add("-Néfertiti");l.Add("-Cléopâtre VI Tryphène");
-        questions.Add(("Lors de quel règne a été bâtie la pierre de Rosette ? ",l,2));
+        questions.Add(("Lors de quel règne a été bâtie la pierre de Rosette ?",l,0));
 
         //Question 5
         l = new List<string>();
-        l.Add("-Ptolémée V ");l.Add("-Ramsès II");l.Add("-Néfertiti");l.Add("-Cléopâtre VI Tryphène"); // ------------ carte
-        questions.Add(("Où a été trouvée la stèle? ?",l,2));
+        l.Add("-Rosette");l.Add("-Le Caire");l.Add("-Karnak");l.Add("-Gizeh");
+        questions.Add(("Où a été trouvée la stèle ?",l,0));
 
-        //Questiob 6
+        //Question 6
         l = new List<string>();
-        l.Add("- 1803");l.Add("- 1795");l.Add("- 1799"); l.Add("- 1802");
+        l.Add("-1803");l.Add("-1795");l.Add("-1799"); l.Add("-1802");
         questions.Add(("Quand la pierre de rosette a été trouvée ?", l, 2));
+
+        //Question 7
+        l = new List<string>();
+        l.Add("-Attaquer les lignes commerciales anglaises"); l.Add("-Coloniser l'Egypte"); l.Add("-Il s'est perdu"); l.Add("-La réponse D");
+        questions.Add(("Que faisait Napoléon en Egypte en 1799 ?", l, 0));
+
+        //Question 8
+        l = new List<string>();
+        l.Add("-1821"); l.Add("-1823"); l.Add("-1820"); l.Add("-1822");
+        questions.Add(("En quelle année les hiéroglyphes ont été déchiffrés ?", l, 3));
+
+        //Question 9
+        l = new List<string>();
+        l.Add("-Analyse Quantitative"); l.Add("-Analyse Qualitative"); l.Add("-Analyse Spéculative"); l.Add("-Cryptographie");
+        questions.Add(("Quelle méthode Jean-François Champollion a utilisé pour déchiffrer les hiéroglyphes ?", l, 0));
+
+        //Question 10
+        l = new List<string>();
+        l.Add("-4"); l.Add("-8"); l.Add("-10"); l.Add("-6");
+        questions.Add(("Combien de langues connaissait Champollion lorsqu'il était jeune ?", l, 1));
+
 
         quiz.SetActive(false);
         displayAnswers.SetActive(false);
-        Image img = carte.GetComponent<Image>();
+        img = carte.GetComponent<Image>();
         carte.SetActive(false);
 
     }
@@ -99,18 +120,20 @@ public class Quiz : MonoBehaviour
     IEnumerator FadeIn(){
         yield return new WaitForSeconds(8f);
         carte.SetActive(true);
+        displayAnswers.SetActive(false);
         for (float ft = 0f; ft<=1f; ft +=.1f){
             Color c = img.color;
             c.a = ft;
             img.color = c;
             Debug.Log(ft);
-            yield return null; 
+            yield return new WaitForSeconds(.1f);
         }
     }
 
     public void Nice()
     {
         surprise.SetActive(true);
+        carte.SetActive(false);
     }
 
     private void DisplayAnswers() {
@@ -131,7 +154,7 @@ public class Quiz : MonoBehaviour
         else {
             displayAnswers.GetComponent<TMP_Text>().text = "Vos connaissances laissent à désirer; vous n'avez répondu qu'à " +c+" questions sur "+questions.Count+". N'hésitez pas à visiter à nouveau le musée et à retenter votre chance!";
         }
-    
+        reponses.answers.Clear();
     }
 
 } /*
